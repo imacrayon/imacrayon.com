cdfd52a74df37dcf9de1f00f7947f4006aa6c0f1
Christian Taylor <christianbtaylor@gmail.com>
2020-05-25 12:11:57 -0500

    Remove redundant link titles

--- source/_nav/menu-responsive.blade.php
+++ source/_nav/menu-responsive.blade.php
@@ -2,7 +2,6 @@
     <ul class="container max-w-3xl mx-auto">
         <li class="px-6">
             <a
-                title="{{ $page->siteName }} About"
                 href="/about"
                 class="block {{ $page->isActive('/about') ? 'line-through text-gray-500 hover:text-gray-50' : 'text-gray-800' }}"
             >About</a>
