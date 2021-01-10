319a90d4043d674a72c7f58d934261e4f1f808dc
Christian Taylor <christianbtaylor@gmail.com>
2021-01-10 13:18:10 -0600

    Remove pagination

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,14 +1,9 @@
----
-pagination:
-  collection: posts
----
-
 @extends('_layouts.base')
 
 @section('content')
 <div class="py-12 px-4 sm:px-6">
   <ul class="text-xl max-w-prose mx-auto space-y-12">
-    @foreach ($pagination->items as $post)
+    @foreach ($posts as $post)
       <x-post-item :post="$post" />
     @endforeach
   </ul>
