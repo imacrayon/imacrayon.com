d71c8db91e837a0d886b83dd6ae123bbcc14a5a0
Christian Taylor <christianbtaylor@gmail.com>
2023-02-03 15:57:07 -0600

    Migrate layouts

--- source/_components/picture-date.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['picture'])
-
-<time {{ $attributes->merge(['class' => 'block text-gray-500 text-sm']) }} datetime="{{ $picture->getDate()->format(DATE_W3C) }}">
-  {{ $picture->getDate()->format('Y') }}
-</time>
--- source/_components/post-item.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['post'])
-
-<li class="relative overflow-hidden space-y-2">
-  <h1 class="text-2xl sm:text-3xl font-medium break-words">
-    <a href="{{ $post->getUrl() }}" class="inline hover:bg-gold-100 hover:text-gold-800 group-focus:text-gold-800">{{ $post->title }}</a>
-  </h1>
-  <x-post-date :post="$post" />
-</li>
